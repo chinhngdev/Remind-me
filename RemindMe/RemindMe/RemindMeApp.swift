18abccf9ae8e75c253e75ea3bdd4e2e6b9cbbd33
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-10-29 22:10:07 +0700

    Initial project setup for RemindMe app

---
+++ RemindMe/RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Chinh on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
